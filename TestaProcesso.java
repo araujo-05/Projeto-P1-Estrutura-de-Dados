910ed42b547d77693c1dfa73c79869189a0914c2
Abraão Castro <abraao.ar05@gmail.com>
2026-04-14 20:03:13 -0300

    Update TestaProcesso.java

--- TestaProcesso.java
+++ TestaProcesso.java
@@ -1,6 +1,9 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
+import java.lang.NullPointerException;
+import java.util.InputMismatchException;
+
 public class TestaProcesso {
     public static void main(String[] args) {
         VetorDinamico v = new VetorDinamico();
@@ -11,46 +14,55 @@ public class TestaProcesso {
         String data = "";
         int i = 0;
         int j =1;
-        while (j>0) {
-            System.out.print("\n\nSelceione a operação desejada\n1-Adicionar\n2-Remover\n3-Mostrar Processos\n0-Encerra\nEscolha:");
-            j = sc.nextInt();
-            sc.nextLine();
-            switch(j){
-                case 1:
-                    System.out.print("Digite o nome do solicitante: ");
-                    solicitante = sc.nextLine();
+		 while (j>0){
+            try{
+                System.out.print("\nSelceione a operação desejada\n1-Adicionar\n2-Remover\n3-Mostrar Processos\n0-Encerra\nEscolha:");
+                j = sc.nextInt();
+                sc.nextLine();
+                switch(j){
+                    case 1:
+                        System.out.print("Digite o nome do solicitante: ");
+                        solicitante = sc.nextLine();
 
-                    System.out.print("Digite o tipo do serviço: ");
-                    tipo = sc.nextLine();
+                        System.out.print("Digite o tipo do serviço: ");
+                        tipo = sc.nextLine();
 
-                    System.out.print("Digite a prioridade do serviço: ");
-                    prioridade = sc.nextInt();
+                        System.out.print("Digite a prioridade do serviço(1 - Baixa | 2 - Normal | 3 - Urgente): ");
+                        prioridade = sc.nextInt();
 
-                    Calendar cal = Calendar.getInstance();
-                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-                    data = formato.format(cal.getTime());
+                        Calendar cal = Calendar.getInstance();
+                        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                        data = formato.format(cal.getTime());
 
-                    v.adiciona(new Processo(solicitante,tipo,prioridade, data));
-                    System.out.println("\nCapacidade do Array: " + v.getCapacidade()+"\nOcupação: "+v.getOcupacao()+"\n");
+                        v.adiciona(new Processo(solicitante,tipo,prioridade, data));
+                        System.out.println("\nCapacidade do Array: " + v.getCapacidade()+"\nOcupação: "+v.getOcupacao()+"\n");
 
-                    sc.nextLine();
-                    break;
-                case 2:
-                    String temp = v.remove();
-                    System.err.println(temp);
-                    System.out.println("\nCapacidade do Array: " + v.getCapacidade()+"\nOcupação: "+v.getOcupacao()+"\n");
-                    break;
-                case 3:
-                    System.out.print("Lista de Processos\n"+v.listar());                    
-                    break;
-                default:
-                    j=0;
-                    break;
+                        sc.nextLine();
+                       
+                        break;
+                    case 2:
+                        String temp = v.remove();
+                        System.err.println(temp);
+                        System.out.println("\nCapacidade do Array: " + v.getCapacidade()+"\nOcupação: "+v.getOcupacao()+"\n");
+                        break;
+                    case 3:
+                        System.out.print("Lista de Processos\n"+v.listar());                    
+                        break;
+                    default:
+                        j=0;
+                        break;
+                }
+                
+            } catch(InputMismatchException e){
+                System.out.println("\n[ERRO] Digite o valor correto");
+                sc.nextLine();
             }
-            
-        }
-
-        System.out.print(v.listar());
+        } 
          sc.close();
     }
 }
+
+/*public class MyExcep(){
+
+}
+*/
